6dbc50a2584495d9be1591c01a6524284febded8
kevin578 <kbriggs@aapl000894.local>
2017-10-29 17:06:50 -0400

    all three user stories working

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const os = require('os')
+
+var  app = express();
+app.set('port', process.env.PORT || 3000);
+
+app.get('/', (req, res) => {
+
+var headerOS = req.headers['user-agent'];
+var start = headerOS.indexOf('(') + 1;
+var end = headerOS.indexOf(')');
+var system = headerOS.substring(start, end);
+
+res.send({
+
+
+
+
+  "ip-address": os.networkInterfaces().lo0[0].address,
+  "language": req.headers['accept-language'].split(',')[0],
+  "software": system
+});
+
+
+});
+
+
+app.listen(app.get('port', ), () => {
+  console.log('Server has started ')
+})
